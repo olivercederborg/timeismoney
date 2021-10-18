ff837eb47b0a2153cf424fc2f2f94d6d5228a783
Oliver Cederborg <hi@olivercederborg.com>
2021-10-18 18:01:52 +0200

    feat: add .env autocompletion

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,21 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import { DefineComponent } from 'vue'
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+	readonly VITE_FB_APIKEY: string
+	readonly VITE_FB_AUTHDOMAIN: string
+	readonly VITE_FB_PROJECTID: string
+	readonly VITE_FB_STORAGEBUCKET: string
+	readonly VITE_FB_MESSAGING_SENDERID: string
+	readonly VITE_FB_APPID: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
 }
