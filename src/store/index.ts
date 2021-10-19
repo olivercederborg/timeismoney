3f4dc77f1bc611d5012c423da22026151c02973e
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:48:04 +0200

    feat: update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,37 @@
+import { User } from 'types/index'
 import { reactive } from 'vue'
-import { User } from '../types'
 
 type State = {
 	user: User | null
+	isLoading: boolean
 }
 
 const store = {
 	debug: true,
 
 	state: reactive<State>({
-		user: null
+		user: null,
+		isLoading: true
 	}),
 
-	setUser(newValue: User | null) {
+	setUser(newValue: State['user']) {
 		if (this.debug) {
 			console.log('setUser triggered with', newValue)
 		}
 
 		this.state.user = newValue
 	},
+	setLoading(newValue: boolean) {
+		if (this.debug) {
+			console.log('setLoading triggered with', newValue)
+		}
+		this.state.isLoading = newValue
+	},
 	getUser() {
 		return this.state.user
+	},
+	getLoading() {
+		return this.state.isLoading
 	}
 }
 
