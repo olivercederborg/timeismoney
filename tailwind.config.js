c3bf7cb77a20baa6fd641391a6e59872f68a3ada
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:37:16 +0200

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				dark1: '#151B31',
+				dark2: '#1A2037',
+				dark3: '#232B41',
+				dark4: '#2F3651'
+			}
+		}
 	},
 	variants: {
 		extend: {}
