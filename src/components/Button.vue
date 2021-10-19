576842e3dfb53f51de739d9226417938ea2f46b4
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:39:22 +0200

    feat: update variants

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,15 +4,18 @@
 		variant?: keyof typeof Variants
 	}>()
 	const Variants = {
-		purple: 'bg-purple-400',
-		green: 'bg-green-400',
-		red: 'bg-red-400',
-		yellow: 'bg-yellow-400'
+		purple: 'bg-purple-400 hover:bg-purple-500',
+		green: 'bg-green-400 hover:bg-green-500',
+		red: 'bg-red-400 hover:bg-red-500',
+		yellow: 'bg-yellow-400 hover:bg-yellow-500'
 	}
 </script>
 
 <template>
-	<button class="px-4 py-2 rounded-lg" :class="[variant && Variants[variant]]">
+	<button
+		class="px-4 py-2 transition-colors rounded-lg"
+		:class="[variant && Variants[variant]]"
+	>
 		{{ props.title }}
 	</button>
 </template>
