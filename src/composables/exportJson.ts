f69ff156ec0ad77b815188666d78156a1ce4987d
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:42:17 +0200

    refactor: minor improvements

--- src/composables/exportJson.ts
+++ src/composables/exportJson.ts
@@ -1,18 +1,15 @@
 import { msToTime } from './msToTime'
 import { jsPDF } from 'jspdf'
 import autoTable from 'jspdf-autotable'
+import { Session } from 'types/index'
 
-export const exportJson = (sessions: any[]) => {
+export const exportJson = (sessions: Session[]) => {
 	const doc = new jsPDF('p', 'pt', 'a4', true)
 
-	let totalTime = 0
-
-	sessions.forEach(item => {
-		totalTime += parseInt(item.rawTime)
-	})
+	const totalTime = sessions.reduce((acc, item) => acc + item.rawTime, 0)
 
 	const tabledSessions = sessions.map((item, i) => {
-		return [`${i + 1}`, item.time]
+		return [`${i + 1}`, item.formattedTime]
 	})
 
 	const totalHours = totalTime / 1000 / 60 / 60
@@ -24,7 +21,7 @@ export const exportJson = (sessions: any[]) => {
 			...tabledSessions,
 			[
 				`Total sessions: ${sessions.length}`,
-				`Total time: ${msToTime(totalTime)}`
+				`Total time: ${msToTime(totalTime, 'long')}`
 			],
 			['Total pay', `$${totalPay.toFixed(1)}`]
 		]
