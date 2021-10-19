23f723a0905cf89181bcf3f656cf6c57c373a242
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:48:52 +0200

    feat: update projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
 	import { ref, onMounted, onUnmounted, watchEffect } from 'vue'
 	import { collection, onSnapshot, query, where } from '@firebase/firestore'
-	import { useRouter } from 'vue-router'
-	import ProjectCard from '../components/ProjectCard.vue'
-	import { db } from '../lib/firebase'
 	import { useUser } from '../store/hooks'
+	import { db } from '../lib/firebase'
+	import { Project } from 'types/index'
+	import ProjectCard from 'components/ProjectCard.vue'
+	import {
+		onBeforeRouteLeave,
+		onBeforeRouteUpdate,
+		useRouter
+	} from 'vue-router'
+	import Button from 'components/Button.vue'
 
 	const isMounted = ref<boolean>(false)
-	const projects = ref<any[]>([])
-
-	const user = useUser()
+	onMounted(() => (isMounted.value = true))
+	const projects = ref<Project[] | undefined | []>([])
+	const { user, isLoading } = useUser()
+	const router = useRouter()
 
 	watchEffect(() => {
 		if (user.value) {
 			const projectsQuery = query(
 				collection(db, 'projects'),
-				where('userId', '==', user.value?.uid)
+				where('userId', '==', user.value.uid)
 			)
 			const unsub = onSnapshot(projectsQuery, snapshot => {
 				projects.value = snapshot.docs.map(doc => ({
@@ -23,20 +30,25 @@
 					...doc.data()
 				}))
 			})
-			onUnmounted(() => unsub)
+			onUnmounted(unsub)
 		}
 	})
 
-	onMounted(() => (isMounted.value = true))
+	watchEffect(() => {
+		if (!isLoading.value && !user.value) router.push('/')
+	})
 </script>
 
 <template>
 	<section class="flex flex-col w-full mt-12">
-		<h2 class="text-3xl font-bold">Your Projects</h2>
+		<header class="flex items-center justify-between">
+			<h2 class="text-3xl font-bold">Your Projects</h2>
+			<Button variant="purple" title="Add Project" />
+		</header>
 
 		<section v-if="user" class="grid w-full h-full grid-cols-3 gap-5 mt-12">
 			<div v-for="project in projects">
-				{{ project.userId }}
+				<ProjectCard :project="project" />
 			</div>
 		</section>
 	</section>
