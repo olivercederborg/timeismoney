8aa5e16dd47e10c81de59f6c42a492c9b7afcc54
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:48:22 +0200

    feat: add types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,27 @@
+import { currencyOptions } from 'types/constants'
+
 export type User = {
-	uid?: string | null
+	uid: string | null | undefined
 	displayName?: string | null
 	email?: string | null
 	photoURL?: string | null
 	authProvider?: string
+	name?: string
+}
+
+export type Project = {
+	id?: string
+	name?: string
+	client?: string
+	userId?: string
+	sessions?: Session[] | null | undefined
+	hourlyRate?: {
+		amount?: number
+		currency?: typeof currencyOptions[number]
+	}
+}
+
+export type Session = {
+	formattedTime: string
+	rawTime: number
 }
