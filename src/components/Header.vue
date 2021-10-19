b32d3334eab5dd81881cf3ee7aa8110d1b01b4a3
Oliver Cederborg <hi@olivercederborg.com>
2021-10-19 06:39:29 +0200

    feat: update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,75 +1,63 @@
 <script setup lang="ts">
 	import { computed } from 'vue'
-	import { useRoute } from 'vue-router'
+	import { useRoute, useRouter } from 'vue-router'
 	import { signUpWithGithub, logOut } from '../lib/db-admin'
 	import { useUser } from '../store/hooks'
+	import Button from 'components/Button.vue'
 
 	const route = useRoute()
-	const user = useUser()
+	const router = useRouter()
+	const { user } = useUser()
 
-	const activeStyles =
-		'text-purple-400 border-b-2 border-purple-400 font-semibold'
+	const activeStyles = 'text-purple-400 font-semibold'
 
 	const login = () => signUpWithGithub()
-	const logout = () => logOut()
+	const logout = () => {
+		logOut()
+		router.replace('/')
+	}
 </script>
 
 <template>
-	<header
-		class="max-w-7xl flex items-center justify-between w-full py-6 mx-auto"
-	>
-		<h1 class="text-xl font-bold tracking-wider text-purple-400 uppercase">
-			TimeIsMoney {{ user?.displayName }}
-		</h1>
-		<nav>
-			<ul class="flex items-center space-x-4">
-				<li v-if="user">
+	<header class="bg-dark1">
+		<div
+			class="max-w-7xl flex items-center justify-between w-full h-16 mx-auto"
+		>
+			<h1 class="text-xl font-bold tracking-wider text-purple-400 uppercase">
+				TimeIsMoney
+			</h1>
+			<nav class="h-full">
+				<div
+					v-if="user"
+					class="flex items-center h-full space-x-4 overflow-hidden"
+				>
 					<router-link
 						:class="route.path === '/' && activeStyles"
-						class="p-3"
+						class="px-2 py-5"
 						to="/"
 						>Home</router-link
 					>
-				</li>
-				<li v-if="user">
 					<router-link
 						:class="route.path === '/projects' && activeStyles"
-						class="p-3"
+						class="px-2 py-5"
 						to="/projects"
 						>Projects</router-link
 					>
-				</li>
-				<li v-if="!user">
-					<button
-						class="
-							px-3
-							py-1
-							text-[15px]
-							font-semibold
-							bg-purple-400
-							rounded-full
-						"
-						@click="login"
-					>
-						Continue with Github
-					</button>
-				</li>
-				<li v-if="user">
-					<button
-						class="
-							px-3
-							py-1
-							text-[15px]
-							font-semibold
-							bg-purple-400
-							rounded-full
-						"
+					<Button
+						variant="purple"
+						title="Log
+					out"
 						@click="logout"
-					>
-						Log out
-					</button>
-				</li>
-			</ul>
-		</nav>
+					/>
+				</div>
+				<div v-else class="flex items-center h-full">
+					<Button
+						variant="purple"
+						title="Continue with Github"
+						@click="login"
+					/>
+				</div>
+			</nav>
+		</div>
 	</header>
 </template>
